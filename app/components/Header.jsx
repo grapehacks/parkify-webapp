ece13436621d52e1bb5c384a2feb2f45b526c82d
Krzysztof Skrzyszewski <kskrzyszewsk@avaya.com>
2016-08-19 22:00:19 +0200

    Header

--- app/components/Header.jsx
+++ app/components/Header.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import {Link} from 'react-router';
 /* eslint-disable */
 import styles from './Header.scss';
 /* eslint-enable */
@@ -7,8 +8,22 @@ class Header extends React.Component {
     render() {
         return (
             <div className='gp-header'>
-                Header
-                {this.props.children}
+                <div className="menu">
+                    <div className="logo pull-left"></div>
+                    <div className="icons pull-right">
+                        <Link to='/login' activeClassName='active'>
+                            <i className="fa fa-sign-out" aria-hidden="true"></i>
+                        </Link>
+                        <Link to='/app/settings' activeClassName='active'>
+                            <i className="fa fa-cog" aria-hidden="true"></i>
+                        </Link>
+                        <Link to='/app/messages' activeClassName='active'>
+                            <i className="fa fa-commenting" aria-hidden="true">
+                                <div className="notification off">12</div>
+                            </i>
+                        </Link>
+                    </div>
+                </div>
             </div>
         )
     }
