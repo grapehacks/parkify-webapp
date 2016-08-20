3b40855a769279c4656bd6e2ee21b24813391933
KrSk <krsk@grapeup.com>
2016-08-20 04:16:47 +0200

    Change to server

--- app/config/api.jsx
+++ app/config/api.jsx
@@ -1,12 +1,13 @@
 import subscriptionAPI from './api/subscriptionAPI.jsx';
 import authAPI from './api/authAPI.jsx';
 import messagesAPI from './api/messagesAPI.jsx';
-const URL = 'http://10.20.1.104:8080';
+//const URL = 'http://10.20.1.104:8080';
+const URLG = 'http://krk.grapeup.com:8080';
 
 const API = {
-    subscriptionAPI: subscriptionAPI(URL),
-    authAPI: authAPI(URL),
-    messagesAPI: messagesAPI(URL)
+    subscriptionAPI: subscriptionAPI(URLG),
+    authAPI: authAPI(URLG),
+    messagesAPI: messagesAPI(URLG)
 };
 
 export default API;
