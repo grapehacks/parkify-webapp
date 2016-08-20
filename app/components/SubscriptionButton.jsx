3fb636095526c8b557559ea2d9771ab5fda9ea4b
tcwiek <tocw@grapeup.com>
2016-08-20 02:23:22 +0200

    Subscription modals added with little (80%) help of Radik

--- app/components/SubscriptionButton.jsx
+++ app/components/SubscriptionButton.jsx
@@ -1,19 +1,40 @@
 import React from 'react';
+import ModalSubscribe from './ModalSubscribe';
+import ModalUnsubscribe from './ModalUnsubscribe';
 /* eslint-disable */
 import styles from './SubscriptionButton.scss';
 /* eslint-enable */
 
 class SubscriptionButton extends React.Component {
+    constructor(props){
+        super(props);
+
+        this.state = {
+            showSubscribeModal: false,
+            showUnsubscribeModal: false
+        }
+    }
+
+    handleClick(){
+        if (this.props.subscribe) {
+            this.setState({showUnsubscribeModal:true})
+        } else {
+            this.setState({showSubscribeModal:true})
+        }
+    }
+
     render() {
         const subscribeClass = this.props.subscribe ? 'on turn-button' : 'off turn-button';
         const rememberLastChoice = this.props.subscribe ? 'on always' : 'off always';
         const rememberLastChoiceClass = this.props.rememberLastChoice ? rememberLastChoice : rememberLastChoice + ' dn';
         return (
-            <div onClick={() => {this.props.handleClick()}} className={subscribeClass}>
+            <div onClick={() => {this.handleClick()}} className={subscribeClass}>
                 <div className="icon"></div>
                 <div className={rememberLastChoiceClass}>
                     <i className="fa fa-refresh fa-spin" aria-hidden="true"></i>
                 </div>
+                <ModalSubscribe visible={this.state.showSubscribeModal} onClose={() => {this.setState({showSubscribeModal:false})}} onConfirm={() => {this.props.handleClick()}}/>
+                <ModalUnsubscribe visible={this.state.showUnsubscribeModal} onClose={() => {this.setState({showUnsubscribeModal:false})}} onConfirm={() => {this.props.handleClick()}}/>
             </div>
         )
     }
